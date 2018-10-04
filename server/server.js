ec1455ae2b02a215fd6b85bdd3524131b6381c00
Yura <yura@Yuras-MacBook-Air.local>
2018-10-04 23:38:38 +0300

    css

--- server/server.js
+++ server/server.js
@@ -15,12 +15,17 @@ app.post('/form',urlencodedParser,function(req,res){
   if (!req.body) {return res.sendStatus(400)}
   let obj = {
     email: 'test@gmail.com',
+    name: 'neme',
     password: '123qwe'
   }
   obj.email = req.body.email;
+  obj.name = req.body.text;
   obj.password = req.body.password;
+
     console.log(req.body.email);
+    console.log(req.body.text);
     console.log(req.body.password);
+    console.log(obj);
 
     res.render('formAnswere',{obj:obj});
 
