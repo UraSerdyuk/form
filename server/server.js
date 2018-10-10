f729098ee678dc82c626a8b797a24be8bf98d03f
Yura <yura@Yuras-MacBook-Air.local>
2018-10-10 15:47:40 +0300

    re.. json

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
 app.set('view engine','ejs');
 
-app.listen(3000,listening);
-function listening () {
-    console.log('server listening ...');
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+// app.listen(3000,listening);
+// function listening () {
+//     console.log('server listening ...');
+// }
 app.use(express.static('test'));
 
 app.post('/form',urlencodedParser,function(req,res){
